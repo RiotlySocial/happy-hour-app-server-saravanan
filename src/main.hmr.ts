0305188f10662e29df986f09f2bc7c0ee49de18e
Saravanan <sarav.majestic@gmail.com>
2018-11-10 07:37:17 +0530

    Enable cors

--- src/main.hmr.ts
+++ src/main.hmr.ts
@@ -2,9 +2,11 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 const PORT = process.env.PORT || 3001;
 declare const module: any;
+import * as cors from 'cors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(cors());
   app.setGlobalPrefix('api');
   await app.listen(PORT);
 
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 const PORT = process.env.PORT || 3001;
+import * as cors from 'cors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(cors());
   app.setGlobalPrefix('api');
   await app.listen(PORT);
 }
